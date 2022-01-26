80484010229006450708e38826ca52ce367b4916
MiryamLaureano <micelaco@gmail.com>
2022-01-26 11:15:52 +0100

    ejemplos leaflet

--- js/farmacias.js
+++ js/farmacias.js
@@ -3,6 +3,8 @@ var urlFarmacias = "datos/farmacias.geojson";
 
 function addDatosFarmacias() {
 
+        var puntosCluster = L.markerClusterGroup();
+
         layerFarmacias  = new L.GeoJSON.AJAX(urlFarmacias, {
             onEachFeature: function (feature, layer) {
                 popupContent = "<b>" + feature.properties.EQUIPAMENT + "</b>"+
@@ -12,6 +14,7 @@ function addDatosFarmacias() {
                 layer.bindPopup(popupContent);
             },
             pointToLayer: function (feature, latlng) {
+                puntosCluster.addLayer(L.marker(latlng));
                 return L.circleMarker(latlng, {
                     radius: 6,
                     fillColor: "#00ff00",
@@ -24,6 +27,25 @@ function addDatosFarmacias() {
         }).addTo(map);
 
         map.setView([41.399733,2.168598],13);
-        // controlCapas.addOverlay(layerFarmacias,"Farmacias");
+        controlCapas.addOverlay(layerFarmacias,"Farmacias");
 
-}//fin funcion
+        controlCapas.addOverlay(puntosCluster,"Cluster");
+
+        var searchControl = new L.Control.Search({
+            layer: layerFarmacias,
+            initial:false,
+            propertyName: 'EQUIPAMENT',
+            circleLocation: true,
+            moveToLocation: function (latlng) {
+                map.setView(latlng, 17);
+            }
+        });
+
+        searchControl.on('search:locationfound', function(e) {
+            e.layer.openPopup();
+        });
+        map.addControl(searchControl);
+
+
+} //fin funcion
+

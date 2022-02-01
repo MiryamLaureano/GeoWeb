e7e77245d46a901ef13f872fb81d3ef376cf5422
MiryamLaureano <micelaco@gmail.com>
2022-02-01 11:22:49 +0100

    ejemplos

--- js/rutas.js
+++ js/rutas.js
@@ -19,6 +19,22 @@ function addRutas() {
 
 } //fin funcion
 
+var animacion;
+function rotarCamara(timestamp) {
+
+    rotacion =timestamp /100 ==360 ?0 : timestamp /100;
+    map.rotateTo(rotacion, { duration: 0 });
+
+    animacion = requestAnimationFrame(rotarCamara);
+}
+
+
+function finalRotarCamara() {
+
+    cancelAnimationFrame(animacion);
+}   
+
+
 function zoomToRutas(valores) {
 
     var coord = valores.split("/");
